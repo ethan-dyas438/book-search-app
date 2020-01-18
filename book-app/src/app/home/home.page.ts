dd5372a8808d7aec946aab30bd5ce1c19530ff9b
ethan-dyas438 <esdyas99@gmail.com>
2020-01-18 15:02:14 -0600

    Added code to display results and seller logos

--- book-app/src/app/home/home.page.ts
+++ book-app/src/app/home/home.page.ts
@@ -7,11 +7,19 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
   book: string = "";
+  priceResults = [
+    {
+      seller: '../../assets/amazon-logo-rgp.webp',
+      rent_price: "26",
+      buy_used_price: "50",
+      buy_new_price: "100"
+    }
+  ];
 
   constructor() {}
 
   searchBook() {
-    console.log(this.book);
+    // resutls = await call;
   }
 
 }
