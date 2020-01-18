6a3beb07e8acbcd3c8cc7f1ffdb80682b3f34d33
ethan-dyas438 <esdyas99@gmail.com>
2020-01-18 13:48:41 -0600

    Added a searchbar

--- book-app/src/app/home/home.page.ts
+++ book-app/src/app/home/home.page.ts
@@ -6,7 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  book: string = "";
 
   constructor() {}
+
+  searchBook() {
+    console.log(this.book);
+  }
 
 }
